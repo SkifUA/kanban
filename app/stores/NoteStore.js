990c256a90d7d67373fd1e2589cecacb278dc126
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-08-12 21:59:48 +0300

    added LocalStorage

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -5,18 +5,10 @@ import NoteActions from '../actions/NoteActions';
 export default class NoteStore {
     constructor() {
 
-        this.bindActions(NoteActions);
+      this.bindActions(NoteActions);
 
-        this.notes = [
-            {
-                id: uuid.v4(),
-                task: 'Learn React'
-            },
-            {
-                id: uuid.v4(),
-                task: 'Do laundry'
-            }
-        ];
+      this.notes = [];
+
     }
 
     create(note) {
