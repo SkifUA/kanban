f9347a5adb74e6abaacac336e19d67bb58e43a7a
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-08-13 22:48:56 +0300

    Added Drag and Drop

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Note from './Note';
 
 import Editable from './Editable';
+import LaneActions from '../actions/LaneActions';
+
 
 
 export default ({
@@ -11,14 +13,21 @@ export default ({
                 }) => (
   <ul className="notes">{notes.map(({id, editing, task}) =>
     <li key={id}>
-      <Note className="note" onClick={onNoteClick.bind(null, id)}>
-        <Editable
-          editing={editing}
-          value={task}
-          onEdit={onEdit.bind(null, id)} />
-        <button
-            className="delete"
-            onClick={onDelete.bind(null, id)}>x</button>
+      <Note className="note" id={id}
+            editing={editing}
+
+            onClick={onNoteClick.bind(null, id)}
+            onMove={LaneActions.move}>
+
+      <Editable
+            editing={editing}
+            value={task}
+            onEdit={onEdit.bind(null, id)} />
+          <button
+              className="delete"
+              onClick={onDelete.bind(null, id)}>
+            x
+          </button>
       </Note>
     </li>
   )}</ul>
