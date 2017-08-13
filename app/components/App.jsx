6eba8d40d6001e82fc4f2ddf6cded3d7fd33203e
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-08-13 11:47:12 +0300

    Added Lanes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,70 +1,27 @@
 import React from 'react';
-import Notes from './Notes';
 import uuid from 'uuid';
-
 import connect from '../libs/connect';
-import NoteActions from '../actions/NoteActions';
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
-  }
+const App = ({LaneActions, lanes}) => {
+  const addLane = () => {
+    LaneActions.create({
+      id: uuid.v4(),
+      name: 'New lane'
+    });
+  };
 
-  addNote = () => {
+  return (
+    <div>
+      <button className="add-lane" onClick={addLane}>+</button>
+      <Lanes lanes={lanes} />
+    </div>
+  );
+};
 
-      this.props.NoteActions.create({
-          id: uuid.v4(),
-          task: 'New task'
-      });
-  }
-
-  render() {
-      const {notes} = this.props;
-    return (
-      <div>
-
-        <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes
-          notes={notes}
-          onNoteClick={this.activateNoteEdit}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote}
-        />
-
-      </div>
-    );
-  }
-
-  deleteNote = (id, e) => {
-    // Avoid bubbling to edit
-    e.stopPropagation();
-
-    this.props.NoteActions.delete(id);
-  }
-
-  activateNoteEdit = (id) => {
-    this.props.NoteActions.update({id, editing: true});
-  }
-
-  editNote = (id, task) => {
-    this.props.NoteActions.update({id, task, editing: false});
-  }
-}
-
-export default connect(({notes}) => ({
-    notes
+export default connect(({lanes}) => ({
+  lanes
 }), {
-    NoteActions
+  LaneActions
 })(App)
--- app/components/Provider/setup.js
+++ app/components/Provider/setup.js
@@ -4,7 +4,13 @@ import persist from '../../libs/persist';
 
 import NoteStore from '../../stores/NoteStore';
 
+import LaneStore from '../../stores/LaneStore';
+
+
 export default alt => {
   alt.addStore('NoteStore', NoteStore);
+
+  alt.addStore('LaneStore', LaneStore);
+
   persist(alt, storage(localStorage), 'app');
 }
